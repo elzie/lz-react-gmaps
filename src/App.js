8a1f6685771d57eef0f11f75120e31c719911c0a
Djan K. Blom <djan@live.dk>
2020-05-11 16:20:00 +0200

    react gmaps

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  GoogleMap,
+  LoadScript,
+  Marker,
+  InfoWindow,
+} from '@react-google-maps/api';
+import * as skoleData from './RandersSkoleData.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+/* DOCS: https://react-google-maps-api-docs.netlify.app/ */
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedSchool: null,
+    };
+  }
+  containerStyle = {
+    width: '90vw',
+    height: '90vh',
+  };
+
+  center = {
+    lat: 56.52,
+    lng: 10.04,
+  };
+
+  render() {
+    const { selectedSchool } = this.state;
+    return (
+      <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_KEY}>
+        <GoogleMap
+          mapContainerStyle={this.containerStyle}
+          center={this.center}
+          zoom={11}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {/* Child components, such as markers, info windows, etc. */}
+          {skoleData.features.map((skole) => (
+            <Marker
+              key={skole.properties.Name}
+              position={{
+                lat: skole.geometry.coordinates[1],
+                lng: skole.geometry.coordinates[0],
+              }}
+              onClick={() => {
+                this.setState({ selectedSchool: skole });
+              }}
+            />
+          ))}
+          {selectedSchool && (
+            <InfoWindow
+              position={{
+                lat: selectedSchool.geometry.coordinates[1],
+                lng: selectedSchool.geometry.coordinates[0],
+              }}
+              onCloseClick={() => {
+                this.setState({ selectedSchool: null });
+              }}
+            >
+              <div>
+                <h2>{selectedSchool.properties.skole}</h2>
+                <p></p>
+                <p>Adresse: {selectedSchool.properties.adresse}</p>
+                <p>Postnr: {selectedSchool.properties.post_nr}</p>
+                <p>By: {selectedSchool.properties.by}</p>
+                <p>
+                  Web:{' '}
+                  <a href={selectedSchool.properties.link}>
+                    {selectedSchool.properties.link}
+                  </a>
+                </p>
+              </div>
+            </InfoWindow>
+          )}
+        </GoogleMap>
+      </LoadScript>
+    );
+  }
 }
-
 export default App;
